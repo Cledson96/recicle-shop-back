3c6121aae7e9e9c9d86c67475eb5549ce2f108f0
Cledson <cledson1996@gmail.com>
2022-11-22 22:41:32 -0300

    configuracoes_iniciais

---
+++ src/database/db.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import { MongoClient} from "mongodb";
+
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+try {
+  await mongoClient.connect();
+  console.log("MongoDB conectado!");
+} catch (err) {
+  console.log(err);
+}
+
+const db = mongoClient.db("recicleShop");
