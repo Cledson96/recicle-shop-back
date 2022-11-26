e1c00609c5d1c7bc7d57020eadbc03fdb11d4392
Raphael Borba <raphaelbsx@gmail.com>
2022-11-25 22:15:03 -0300

    feat: getProduct route

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { getProducts } from "../controllers/products.controller.js";
-import { validateUser } from "../Middlewares/validateUser.middleware.js";
 
 const router = Router()
 
-router.get('/products', validateUser, getProducts )
+router.get('/products', getProducts )
 
 
 export default router
