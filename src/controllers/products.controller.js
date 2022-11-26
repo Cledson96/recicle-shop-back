63ec94297f6c3e6f66be0772960bdba4ff29f62d
Raphael Borba <raphaelbsx@gmail.com>
2022-11-25 22:53:07 -0300

    feat: add query in get products route

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -5,10 +5,22 @@ import { products } from "../database/db.js"
 
 export async function getProducts(req, res){
     
+    const category = req.query.category
 
     try {
+
+        if(category){
+            const listProductsCategory= await products.find({category}).toArray()
+
+            res.send(listProductsCategory).status(200)
+            
+            return
+        }
+
         const listProducts = await products.find().toArray()
+        
         res.status(200).send(listProducts)
+
     } catch (error) {
         console.log(error)
         res.sendStatus(500)
